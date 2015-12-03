d0c367807d7842122068362f89bdaa2edf08b0ca
Claire Diehl <claire.diehl@students.olin.edu>
2015-12-03 17:24:04 -0500

    Now with random shape generation :)

--- Groups.py
+++ Groups.py
@@ -2,42 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from Tkinter import *
-
-
-# class buttons:
-#     def __init__(self, master,gps):
-
-#         frame = Frame(master)
-#         frame.grid()
-        # self.listofshapes=[]
-        # self.makeshape = Button(frame, text="Make Shape", command=self.printshape(gps)).grid(row=3,column=0)
-        # self.symmetry = Button(frame, text="Make Symmetry!", command=self.symmetry).grid(row=4,column=0)
-        # self.clearall = Button(frame, text="Clear Shapes", command=self.clearshapes(master)).grid(row=5,column=0)
-        # self.button = Button(frame, text="QUIT", fg="red", command=frame.quit).grid(row=6,column=0)   
-
-        # self.triangleoption = Radiobutton(frame,variable="shape",text="triangle",value="triangle").grid(row=3,column=1,sticky=W)
-        # self.ovaloption = Radiobutton(frame,variable="shape",text="oval",value="oval").grid(row=3,column=2,sticky=W)
-        # self.polygonoption = Radiobutton(frame,variable="shape",text="polygon",value="points").grid(row=3,column=3,sticky=W)
-        # self.blueoption = Radiobutton(frame,variable="color",text="blue",value="blue").grid(row=4,column=1,sticky=W)
-        # self.redoption = Radiobutton(frame,variable="color",text="red",value="red").grid(row=4,column=2,sticky=W)
-        # self.yellowoption = Radiobutton(frame,variable="color",text="yellow",value="yellow").grid(row=4,column=3,sticky=W)
-
-    # def clearshapes(self,master):
-    #     print "Shapes Cleared"
-    #     #ms = MakeShapes(master)
-    #     MakeShapes.canvas.delete(ALL)
-
-    # def printshape(self,gps):
-    #     self.listofshapes=[]
-    #     self.listofshapes.append(shape("points", (100,20,40,40,20,10), "red"))
-    #     self.listofshapes.append(shape("rectangle", (200,20,40,40), "blue"))
-    #     #print listofshapes
-    #     #print len(listofshapes)
-    #     for i in range(len(self.listofshapes)):
-    #         MakeShapes.PlotShape(gps,self.listofshapes[i])
-
-    # def symmetry(self):
-    #     print "MAKE SYMMETRY"
+import random
 
 class shape():
     points=()
@@ -65,12 +30,15 @@ class MakeShapes(Frame):
         self.clearall = Button(frame, text="Clear Shapes", command=self.clearshapes).grid(row=7,column=0)
         self.button = Button(frame, text="QUIT", fg="red", command=frame.quit).grid(row=8,column=0)   
 
-        self.triangleoption = Radiobutton(frame,variable="shape",text="triangle",value="triangle").grid(row=6,column=1,sticky=W)
-        self.ovaloption = Radiobutton(frame,variable="shape",text="oval",value="oval").grid(row=6,column=2,sticky=W)
-        self.polygonoption = Radiobutton(frame,variable="shape",text="polygon",value="points").grid(row=6,column=3,sticky=W)
-        self.blueoption = Radiobutton(frame,variable="color",text="blue",value="blue").grid(row=7,column=1,sticky=W)
-        self.redoption = Radiobutton(frame,variable="color",text="red",value="red").grid(row=7,column=2,sticky=W)
-        self.yellowoption = Radiobutton(frame,variable="color",text="yellow",value="yellow").grid(row=7,column=3,sticky=W)
+        self.shape=StringVar()
+        self.color=StringVar()
+
+        self.triangleoption = Radiobutton(frame,variable=self.shape,text="rectangle",value="rectangle").grid(row=6,column=1,sticky=W)
+        self.ovaloption = Radiobutton(frame,variable=self.shape,text="oval",value="oval").grid(row=6,column=2,sticky=W)
+        self.polygonoption = Radiobutton(frame,variable=self.shape,text="polygon",value="points").grid(row=6,column=3,sticky=W)
+        self.blueoption = Radiobutton(frame,variable=self.color,text="blue",value="blue").grid(row=7,column=1,sticky=W)
+        self.redoption = Radiobutton(frame,variable=self.color,text="red",value="red").grid(row=7,column=2,sticky=W)
+        self.yellowoption = Radiobutton(frame,variable=self.color,text="yellow",value="yellow").grid(row=7,column=3,sticky=W)
 
         self.canvas = Canvas(frame, width=400, height=400,background="white")#.grid(row=2,column=0)
         print self.canvas
@@ -81,8 +49,27 @@ class MakeShapes(Frame):
         self.label= Label(frame, text="Groups!").grid(row=0,column=0,columnspan=4)
         #label.grid(fill=BOTH,expand=1)
 
+    def clearshapes(self):
+        print "Shapes Cleared"
+        #ms = MakeShapes(master)
+        self.canvas.delete(ALL)
+
+    def printshape(self):
+        print self.shape.get()
+        print self.color.get()
+        a=random.uniform(0,400)
+        b=random.uniform(a,400)
+        c=random.randint(0,400)
+        d=random.randint(c,400)
+        self.listofshapes=[]
+        self.listofshapes.append(shape(self.shape.get(), (a,b,c,d), self.color.get()))
+        print self.listofshapes
+        #print len(listofshapes)
+        for i in range(len(self.listofshapes)):
+            self.PlotShape(self.listofshapes[i])
+
     def PlotShape(self,shape):
-        print "PRINT SHAPES"
+        #print "PRINT SHAPES"
         if shape.shapetype == "rectangle":
             self.canvas.create_rectangle(shape.points[0], shape.points[1], shape.points[2], shape.points[3], fill=shape.color)
         if shape.shapetype == "oval":
@@ -91,24 +78,8 @@ class MakeShapes(Frame):
             self.canvas.create_polygon(shape.points, fill=shape.color)
         self.canvas.grid(row=1,column=0,columnspan=2)
 
-    def clearshapes(self):
-        print "Shapes Cleared"
-        #ms = MakeShapes(master)
-        self.canvas.delete(ALL)
-
-    def printshape(self):
-        self.listofshapes=[]
-        self.listofshapes.append(shape("points", (100,20,40,40,20,10), "red"))
-        self.listofshapes.append(shape("rectangle", (200,20,40,40), "blue"))
-        #print listofshapes
-        #print len(listofshapes)
-        for i in range(len(self.listofshapes)):
-            self.PlotShape(self.listofshapes[i])
-
     def symmetry(self):
         print "MAKE SYMMETRY"
-
-#    def Rotate(self):
 
 def main():
     groot = Tk()
